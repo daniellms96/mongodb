5d42c883d4e69e3307833a85912848fbe90dad9c
DaniLomas <daniellomas116@gmail.com>
2025-01-28 17:43:03 +0000

    serverCompletado

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ app.use(express.json());
 
 // Datos de ejemplo (simulando una base de datos).
 let items = [
-  { id: 1, name: "Item 1" },
-  { id: 2, name: "Item 2" },
-  { id: 3, name: "Item 3" },
+  { id: 1, name: "Ordenador 1" },
+  { id: 2, name: "Ordenador 2" },
+  { id: 3, name: "Ordenador 3" },
 ];
 
 
